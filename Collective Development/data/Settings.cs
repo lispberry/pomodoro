e23415bbc2bda735933fbae68cbcbda63301359e
Ilya Volkov <ilya@lispberry.com>
2021-05-31 07:13:05 +0500

    FIP

---
+++ Collective Development/data/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collective_Development.data
+{
+    class Settings
+    {
+        public int DefaultDelayTime { get; set; }
+        public bool BlockKeyBoard { get; set; }
+        public bool AddSound { get; set; }
+    }
+}
